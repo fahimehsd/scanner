e284d710837fe8b2c1e098a4a26d4580e8d224ea
Fahimeh Sadeghi <115643572+fahimehsd@users.noreply.github.com>
2023-08-12 09:13:09 +0330

    scanner

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-function generateQRCode() {
-  let url = document.getElementById("url").value;
-  if (url) {
-    let qrcodeContainer = document.getElementById("qrcode");
-    qrcodeContainer.innerHTML = "";
-    new QRCode(qrcodeContainer, url);
-    document.getElementById("qrcode-container").style.display = "block";
-  } else {
-    alert("Please enter a valid URL");
-  }
+// When scan is successful fucntion will produce data
+function onScanSuccess(qrCodeMessage) {
+  document.getElementById("result").innerHTML =
+    '<span class="result">' + qrCodeMessage + "</span>";
 }
+
+// When scan is unsuccessful fucntion will produce error message
+function onScanError(errorMessage) {
+  // Handle Scan Error
+}
+
+// Setting up Qr Scanner properties
+var html5QrCodeScanner = new Html5QrcodeScanner("reader", {
+  fps: 10,
+  qrbox: 250
+});
+
+// in
+html5QrCodeScanner.render(onScanSuccess, onScanError);
